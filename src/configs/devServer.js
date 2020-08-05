820b3ec76990268cb654daca01434739be41f770
zhouxianglin <zhouxianglin001@lianjia.com>
2020-08-05 16:38:52 +0800

    devserver默认配置

--- src/configs/devServer.js
+++ src/configs/devServer.js
@@ -11,13 +11,13 @@ module.exports = (appPath, env) => {
 		port: PORT,
 		host: 'localhost',
 		quiet: false,
-		clientLogLevel: 'silent',
+		clientLogLevel: 'info',
 		contentBase: path.join(appPath, 'public'), // boolean | string | array, static file location
 		compress: true, // enable gzip compression
 		historyApiFallback: true, // true for index.html upon 404, object for multiple paths
 		hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
 		https: false, // true for self-signed, object for cert authority
-		noInfo: true, // only errors & warns on hot reload
+		noInfo: false, // only errors & warns on hot reload
 	}, devServer)
 
 	return serverConfig
